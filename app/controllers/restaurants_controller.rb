0fc8ca6f75bfeda96ffbf73f622a85d4c4fbc162
Roi Assayag <rockybb777@gmail.com>
2024-03-12 09:42:17 +0100

    pages controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,53 +4,7 @@ class RestaurantsController < ApplicationController
 
   def index
     #top restaurants
-    @restaurants = Restaurant.all
-    @top_restaurants = []
-
-    #calculates average of all restaurants
-    average = (Review.pluck(:rating).sum / Review.count.to_f).round(1)
-
-    # calculates and selects restaurants that are above average
-    @restaurants.each do |restaurant|
-      review_sum = 0
-      restaurant.reviews.each do |review|
-        review_sum += review.rating
-      end
-
-      if (review_sum/restaurant.reviews.length.to_f).round(1) > average
-        @top_restaurants << restaurant
-      end
     end
-
-    # top users
-    @top_users = User.joins(:reviews)
-              .group(:id)
-              .order('COUNT(reviews.id) DESC')
-              .limit(5)
-
-
-    # reviews of the week
-    @followers_reviews = Review.joins(user: :followers)
-                .where(followers: { taster_id: current_user.id} ).order(created_at: :desc)
-                .limit(10)
-
-
-    # search method
-    @restaurants = Restaurant.all
-        if params[:query].present?
-        @restaurants = @restaurants.where("neighborhood ILIKE ?", "%#{params[:query]}%")
-
-  # follow restaurant method
-
-    @user = current.user
-    @restaurant = Restaurant.find(params[:restaurant_id])
-    @bookmark = Bookmark.new(user: @user , restaurant: @restaurant)
-        if @bookmark.save
-        end
-      redirect_to profile_path
-      render :new
-    end
-end
 
   def show
     # calculting the rating of authenthic user as two reviews,
