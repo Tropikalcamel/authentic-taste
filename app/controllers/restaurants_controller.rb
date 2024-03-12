45b076f5035c0becf6aca288b3c24da2576eb1fd
Roi Assayag <rockybb777@gmail.com>
2024-03-12 14:23:25 +0100

    few update

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,6 @@
 class RestaurantsController < ApplicationController
 
-  before_action :set_restaurant, only: %i[ show ]
+  # before_action :set_restaurant, only: %i[ show ]
 
   def index
     #top restaurants
@@ -9,27 +9,29 @@ class RestaurantsController < ApplicationController
   def show
     # calculting the rating of authenthic user as two reviews,
     # and the result is a persantge
-    # @restaurant = Restaurant.find(params[:id])
+    review_sum = 0
+    @restaurant = Restaurant.find(params[:id])
     @restaurant.reviews.each do |review|
       review_sum += review.rating * review.weight
-    end
+      end
     total_weight = @restaurant.reviews.sum(:weight)
     @average_rating = (review_sum / total_weight.to_f).round(1)
 
-    # follower taster method method
-    follower_params = { user_id: current_user.id , taster_id:params[:user_id]}
-    @follower = Follower.new(follower_params)
-      if @follower.save
+    # follower taster method
+
     # show review
     @review = Review.all
-      @review.pluck.order(created_at:  :desc, rating: :desc)
+      @review.order(created_at:  :desc, rating: :desc)
 
-      end
-      end
-
-  private
-
-  def set_restaurant
-    @restaurant = Restaurant.find(params[:restaurant_id])
+      # end
   end
+
+
+
+
+  # private
+
+  # def set_restaurant
+  #   @restaurant = Restaurant.find(params[:restaurant_id])
+  # end
 end
