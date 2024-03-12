05c743d3ad40fd3df30afa1bc226b2c61d740801
tarik <tarik.elmoudden@gmail.com>
2024-03-12 09:26:20 +0100

    add some css to user page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,48 +1,52 @@
 class RestaurantsController < ApplicationController
+# before_action :set_restaurant, only: %i[ show show_review ]
 
-  before_action :set_restaurant, only: %i[ show show_review ]
   def index
-    #top restaurants
-    @restaurants = Restaurant.all
-    @top_restaurants = []
+  # Retrieve all restaurants
+  @restaurants = Restaurant.all
+  @top_restaurants = []
 
-    #calculates average of all restaurants
-    average = (Review.pluck(:rating).sum / Review.count.to_f).round(1)
+  # Calculate the average rating of all reviews
+  average_rating = Review.pluck(:rating).sum / Review.count.to_f
 
-    # calculates and selects restaurants that are above average
+  # Select restaurants that are above the average rating
   @restaurants.each do |restaurant|
-    review_sum = 0
-    restaurant.reviews.each do |review|
-      review_sum += review.rating
-    end
+    review_sum = restaurant.reviews.sum(:rating).to_f
 
-    if (review_sum/restaurant.reviews.length.to_f).round(1) > average
+    if review_sum / restaurant.reviews.length.to_f > average_rating
       @top_restaurants << restaurant
     end
   end
-    # top users
-    top_users = User.joins(:reviews)
-              .group(:id)
-              .order('COUNT(reviews.id) DESC')
-              .limit(5)
 
-    # reviews of the week
-    followers_reviews = Review.joins(user: :followers)
-                .where(followers: { taster_id: 49} ).order(created_at: :desc)
-                .limit(3)
-    # search method
-                @restaurants = Restaurant.all
+  # Retrieve top users
+  @top_users = User.joins(:reviews)
+                    .group(:id)
+                    .order('COUNT(reviews.id) DESC')
+                    .limit(5)
+
+  # Retrieve reviews of the week for a specific user (taster_id: 49)
+  @followers_reviews = Review.joins(user: :followers)
+                             .where(followers: { taster_id: 49 })
+                             .order(created_at: :desc)
+                             .limit(3)
+
+  # Search method
   if params[:query].present?
     @restaurants = @restaurants.where("neighborhood ILIKE ?", "%#{params[:query]}%")
-  # follow method
-    @user = User.find(params [:id])
+  end
+
+  # Follow restaurant method (I'm assuming this logic will be applied elsewhere)
+  # This code snippet seems out of context and might need adjustments
+  if params[:id].present?
+    @user = User.find(params[:id])
     @boomarks = Bookmark.new
-        @bookmark.user = @bookmark
-        if @bookmark.save
+    @bookmark.user = @bookmark
+    if @bookmark.save
       redirect_to profile_path
-      end
+    end
   end
 end
+
 
   def show
     # calculting the rating of authenthic user as two reviews,
@@ -54,18 +58,17 @@ end
     total_weight = @restaurant.reviews.sum(:weight)
     @average_rating = (review_sum / total_weight.to_f).round(1)
   end
-def show_review
+
+  def show_review
     @review = Review.all
     @review.pluck.order(created_at:  :desc, rating: :desc)
+  end
 
-end
 
+  private
 
-private
-
-def set_restaurant
-  @restaurant = Restaurant.find(params[:restaurant_id])
-end
-
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
 
 end
